26852e029741b9c866dae81587b17b4a2cef1d71
Matt Linaberry <matt.linaberry@we-ics.com>
2015-02-11 20:29:59 -0500

    Start day logic...
    Well, started!

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -22,8 +22,10 @@ class ViewController: UIViewController {
     var theSalesman = Salesman()
     var iceCubesPurchased = 0
     var lemonsPurchased = 0
-    var iceCubesMixed = 0
-    var lemonsMixed = 0
+    var iceCubesMixed = 0.0
+    var lemonsMixed = 0.0
+    
+    var theLemonadeRatio:Double = 0.0
     
     let costOfLemon = 2
     let costOfIceCube = 1
@@ -88,22 +90,79 @@ class ViewController: UIViewController {
     }
     
     @IBAction func addMixLemonButtonPressed(sender: AnyObject) {
-        lemonsMixed++
+        if theSalesman.lemonsOnHand >= 1 {
+            lemonsMixed++
+            theSalesman.lemonsOnHand--
+        }
+        else {
+            showAlertWithText(header: "Alert", message: "You need to buy more lemons!")
+        }
         refreshView()
     }
     @IBAction func addMixIceCubeButtonPressed(sender: AnyObject) {
-        iceCubesMixed++
+        if theSalesman.iceCubesOnHand >= 1 {
+            iceCubesMixed++
+            theSalesman.iceCubesOnHand--
+        }
+        else {
+            showAlertWithText(header: "Alert", message: "You need to buy more ice cubes!")
+        }
         refreshView()
     }
     @IBAction func subtractMixLemonButtonPressed(sender: AnyObject) {
-        lemonsMixed--
+        if lemonsMixed > 0 {
+            lemonsMixed--
+            theSalesman.lemonsOnHand++
+        }
         refreshView()
     }
     @IBAction func subtractMixIceCubeButtonPressed(sender: AnyObject) {
-        iceCubesMixed--
+        if iceCubesMixed > 0 {
+            iceCubesMixed--
+            theSalesman.iceCubesOnHand++
+        }
         refreshView()
     }
     @IBAction func startDayButtonPressed(sender: AnyObject) {
+        if iceCubesMixed == 0  {
+            showAlertWithText(header: "Attention!", message: "You must mix at least one ice cube!")
+            return
+        }
+        calculateLemonadeRatio()
+        // create a random number of customers for the day
+        
+//        Then, create a random taste preference(between 0 and 1) for each customer (hint, this should be a constant and you may want to use a type of loop to generate each preference).
+//        For example, you might generate 5 customers for the day with preferences as such: 0.5, 0.7, 0.3, 0.4, 0.1
+//        Then, you should compare your preferences to a range of values, as well as the current lemonade ratio to a separate range of values. Then see if they match. We are not comparing the ratios directly to each other! Ranges are shown in the steps below. So, what do we mean!? Here's a breakdown:
+//        You should compare your randomly-generated customer's preferences to 3 different ranges from 0 to 1.
+//        The three ranges are:
+//        0 to 0.4 – favors acidic lemonade
+//        0.4 to 0.6 – favors equal parts lemonade
+//        0.6 to 1 – favors diluted lemonade
+//        At the same time, you should be comparing your lemonade ratio to a different set of ranges.
+//        Greater than 1 (Acidic Lemonade)
+//        Equal to 1 (Equal Portioned Lemonade)
+//        Less than 1 (Diluted Lemonade)
+//        Here's where it will come together. I will spell out the first set of logic and hopefully you'll be able to do the rest.
+//        For example, If customerPreference is Less than 0.4 AND lemonadeRatio is Greater than 1... Get paid a $1 and print out "Paid!"
+//        This should read, If the customer likes acidic lemonade (between 0 – 0.4) and the lemonade is acidic (greater than 1) Get paid a dollar!
+//        Follow this example for the next 2 comparison sets...
+//        At the end, you'll want an else statement where they didn't want to buy your lemonade, so you will not be paid.
+//        **You'll want to run through this logic for each customer you randomly generate.**
+//        While you are comparing the preferences to these ranges, we want to see which customers are buying and which are not. So, write a println that will show
+//        each customer’s generated preference value, and
+//        whether or not they matched to the lemonade.
+//        If they matched, print “Paid!”
+//        If not, print “No match, No Revenue.”
+        
+        
+        
+    }
+    
+    func calculateLemonadeRatio() {
+        theLemonadeRatio = (lemonsMixed / iceCubesMixed)
+        println("The lemonade ratio is \(theLemonadeRatio)")
+        
     }
     
     func refreshView() {
